ecdcbd87737f40de700e25ca0bfb666c4b2c3b77
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2022-05-19 17:39:12 -0400

    Make walls fake by default

--- ScuffedWalls/Program/Functions/TextToWall.cs
+++ ScuffedWalls/Program/Functions/TextToWall.cs
@@ -84,6 +84,11 @@ namespace ScuffedWalls.Functions
                 _duration = duration,
                 _customData = new TreeDictionary()
             };
+            wall._customData ??= new TreeDictionary();
+        
+            // by default make walls fake and uninteractable
+            wall._customData["_fake"] = true;
+            wall._customData["_interactable"] = false;
             BeatMap.Append(wall, UnderlyingParameters.CustomDataParse(new BeatMap.Obstacle()), BeatMap.AppendPriority.High);
 
             lines.Reverse();
